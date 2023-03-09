97f18b540fb8b3af85c3ba56ef0ee0936a3390f9
MarissaAG <747121@student.dusd.net>
2023-03-09 09:56:35 -0800

    random spawn

---
+++ BasicGame/Assets/Scripts/DestroyOutOfBounds1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds1 : MonoBehaviour
+{
+    private float topBound = 30f;
+    private float lowerBound = -10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        { 
+          Destroy(gameObject);
+        }
+    }
+}
